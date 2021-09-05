265440c3f11e94a3e3bbd1866c4c20d5f6d96c0f
sunflower5 <53665784+sunflower5@users.noreply.github.com>
2021-09-05 13:01:42 +0000

    Deploying to gh-pages from @ sunflower5/portfolio@047b2fb7e5eb73226159a6e524edc6f34bfa4911 🚀

---
+++ _next/static/TLIkPL5YBCHviZS8oSA_1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/273-06f7d167f4ed1a63ac4f.js","static/css/78c3dceb3644c9523699.css","static/chunks/pages/index-a00c431c53494348d8b2.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
